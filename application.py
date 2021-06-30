ee56df0f3dc754fec3e4274587a325b696d2e80f
KARCHEJ <84968741+KARCHEJ@users.noreply.github.com>
2021-06-30 08:22:38 -0400

    Update application.py

--- application.py
+++ application.py
@@ -2,17 +2,17 @@ from flask import Flask
 import os
 from flask import Response
 from flask import request
+import requests
 import json
 app = Flask(__name__)
 
 @app.route("/",methods = ['POST', 'GET'])
 #@app.after_request
-def hello():
+def handleRequest():
     req_data = request.get_json()
-    head = (os.environ["test"])
+    head = (os.environ["apim_token"])
     url = (os.environ["apim_url"])
-    response = Response()
-    response.headers["Ocp-Apim-Subscription-Key"] = head
-    response.data = json.dumps(req_data)
-    postResponse = request.post(url,data = response.data,header = response.headers)
-    return response
+    botResponse = Response(status = 200)
+    headers = {"Ocp-Apim-Subscription-Key" : head, "Content-Type" : "application/json" }
+    postResponse = requests.post(url,data = json.dumps(req_data), headers = headers)
+    return botResponse
